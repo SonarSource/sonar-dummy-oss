897ea7b31d16bd2bcc94fa229a4d71950abab85f
Tom <thomas.verin@sonarsource.com>
2017-01-04 13:19:50 +0100

    test upload

--- travis.sh
+++ travis.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-set -euo pipefail
-
-function installTravisTools {
-  mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
-  source ~/.local/bin/install
-}
-
-installTravisTools
-
-case "$TEST" in
-
-ci)
-  #deploy pull request artifacts to repox to start QA
-  export DEPLOY_PULL_REQUEST=true
-  regular_mvn_build_deploy_analyze  
-  ;;
-
-*)
-  echo "Unexpected TEST mode: $TEST"
-  exit 1
-  ;;
-
-esac
+test-sq-upload/test.sh
