f039a862b9173f5c27bcd64b070e008fb5048ffc
Tom <thomas.verin@sonarsource.com>
2017-01-04 14:22:24 +0100

    add build number

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ cd sonarqube
 wget -c https://repox.sonarsource.com/sonarsource-qa/org/sonarsource/sonarqube/sonar-application/6.3-build16399/sonar-application-6.3-build16399.zip --user=$ARTIFACTORY_QA_READER_USERNAME --password=$ARTIFACTORY_QA_READER_PASSWORD --auth-no-challenge
 cd ..
 # set project version
-export PROJECT_VERSION=1.$TRAVIS_BUILD_ID-$HOSTNAME
+export PROJECT_VERSION=1.$TRAVIS_BUILD_NUMBER-$HOSTNAME
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$PROJECT_VERSION
 
 #upload SQ
