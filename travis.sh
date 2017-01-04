4bcaca72dad17d563a007ae501cf031addea3748
Tom <thomas.verin@sonarsource.com>
2017-01-04 13:24:39 +0100

    use travis tools

--- travis.sh
+++ travis.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
+function installTravisTools {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+
 cd test-sq-upload
 ./test.sh
