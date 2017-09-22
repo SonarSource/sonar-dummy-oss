bd26744ecc210b47ebe947b5d3c10221be6955b6
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-09-22 11:54:54 +0200

    Upgrade to travis-utils v37

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ export DEPLOY_PULL_REQUEST=true
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/f0cf0bc49738a07c20029f405ce2059c1ff3d7a7 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v37 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 installTravisTools
