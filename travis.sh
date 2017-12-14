d0ca7f633ede13ce6ee571bf9818b0183b7a018e
David Rautureau <david.rautureau@sonarsource.com>
2017-12-14 14:55:14 +0100

    Update Build and QA (#15)

--- travis.sh
+++ travis.sh
@@ -7,9 +7,20 @@ export DEPLOY_PULL_REQUEST=true
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v41 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v42 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 installTravisTools
+. ~/.local/bin/installJDK8
+. ~/.local/bin/installMaven35
+
+# When a pull request is open on the branch, then the job related
+# to the branch does not need to be executed and should be canceled.
+# It does not book slaves for nothing.
+# @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
+cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
+
+# allow deployment of PR artifacts to repox with
+export DEPLOY_PULL_REQUEST=true
 
 regular_mvn_build_deploy_analyze
