15296c58d77af4282458194dca51f4c077833fda
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-12-27 10:47:27 +0100

    upgrade to released version of travis-utils v43

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ export DEPLOY_PULL_REQUEST=true
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/feature/sl/use_builtin_pr_decoration | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v43 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 installTravisTools
