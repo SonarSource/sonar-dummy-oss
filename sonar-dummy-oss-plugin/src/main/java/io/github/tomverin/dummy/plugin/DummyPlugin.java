37caf225ec864c9d701bacf740a12d26e48629c4
Samir M <samir.marini@sonarsource.com>
2025-06-18 18:25:37 +0200

    BUILD-8328 adapting package name

--- sonar-dummy-oss-plugin/src/main/java/io/github/tomverin/dummy/plugin/DummyPlugin.java
+++ sonar-dummy-oss-plugin/src/main/java/io/github/tomverin/dummy/plugin/DummyPlugin.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package io.github.tomverin.plugin;
+package io.github.tomverin.dummy.plugin;
 
 import java.util.List;
 import org.sonar.api.SonarPlugin;
